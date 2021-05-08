7c9b177d313b9f67beb109f5ddcb555aa2ea3873
wrap-that-potassium <wrap-that-potassium@protonmail.com>
2021-05-08 16:43:27 +0200

    WIP: Benis introduced in the jungle.

---
+++ contracts/Benis.sol
@@ -0,0 +1,254 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity ^0.6.12;
+
+import '@pancakeswap/pancake-swap-lib/contracts/math/SafeMath.sol';
+import '@pancakeswap/pancake-swap-lib/contracts/token/BEP20/IBEP20.sol';
+import '@pancakeswap/pancake-swap-lib/contracts/token/BEP20/SafeBEP20.sol';
+import '@pancakeswap/pancake-swap-lib/contracts/access/Ownable.sol';
+
+contract Benis is Ownable {
+    using SafeMath for uint256;
+    using SafeBEP20 for IBEP20;
+
+    // Info of each user.
+    struct UserInfo {
+        uint256 amount; // How many LP tokens the user has provided.
+        uint256 rewardDebt; // Reward debt. See explanation below.
+        //
+        // We do some fancy math here. Basically, any point in time, the amount of BANANAs
+        // entitled to a user but is pending to be distributed is:
+        //
+        //   pending reward = (user.amount * pool.accBananaPerShare) - user.rewardDebt
+        //
+        // Whenever a user deposits or withdraws LP tokens to a pool. Here's what happens:
+        //   1. The pool's `accWBANPerShare` (and `lastRewardBlock`) gets updated.
+        //   2. User receives the pending reward sent to his/her address.
+        //   3. User's `amount` gets updated.
+        //   4. User's `rewardDebt` gets updated.
+    }
+
+    // Info of each pool.
+    struct PoolInfo {
+        IBEP20 lpToken; // Address of LP token contract.
+        uint256 allocPoint; // How many allocation points assigned to this pool. BANANAs to distribute per block.
+        uint256 lastRewardBlock; // Last block number that BANANAs distribution occurs.
+        uint256 accWBANPerShare; // Accumulated BANANAs per share, times 1e12. See below.
+    }
+
+    // The wBAN TOKEN!
+    IBEP20 public wban;
+    // wBAN tokens distributed per block.
+    uint256 public wbanRewardPerBlock;
+
+    // Info of each pool.
+    PoolInfo[] public poolInfo;
+    // Info of each user that stakes LP tokens.
+    mapping(uint256 => mapping(address => UserInfo)) public userInfo;
+    // Total allocation points. Must be the sum of all allocation points in all pools.
+    uint256 public totalAllocPoint = 0;
+    // The block number when BANANA mining starts.
+    uint256 public startBlock;
+
+    event Deposit(address indexed user, uint256 indexed pid, uint256 amount);
+    event DepositRewards(uint256 amount);
+    event Withdraw(address indexed user, uint256 indexed pid, uint256 amount);
+    event EmergencyWithdraw(address indexed user, uint256 indexed pid, uint256 amount);
+
+    constructor(
+        IBEP20 _wban,
+        uint256 _wbanRewardPerBlock,
+        uint256 _startBlock
+    ) public {
+        wban = _wban;
+        wbanRewardPerBlock = _wbanRewardPerBlock;
+        startBlock = _startBlock;
+        totalAllocPoint = 1000;
+    }
+
+    modifier validatePool(uint256 _pid) {
+        require(_pid < poolInfo.length, 'validatePool: pool exists?');
+        _;
+    }
+
+    function poolLength() external view returns (uint256) {
+        return poolInfo.length;
+    }
+
+    // Detects whether the given pool already exists
+    function checkPoolDuplicate(IBEP20 _lpToken) public view {
+        uint256 length = poolInfo.length;
+        for (uint256 _pid = 0; _pid < length; _pid++) {
+            require(poolInfo[_pid].lpToken != _lpToken, 'add: existing pool');
+        }
+    }
+
+    // Add a new lp to the pool. Can only be called by the owner.
+    // XXX DO NOT add the same LP token more than once. Rewards will be messed up if you do.
+    function add(
+        uint256 _allocPoint,
+        IBEP20 _lpToken,
+        bool _withUpdate
+    ) public onlyOwner {
+        if (_withUpdate) {
+            massUpdatePools();
+        }
+        checkPoolDuplicate(_lpToken);
+        uint256 lastRewardBlock = block.number > startBlock ? block.number : startBlock;
+        totalAllocPoint = totalAllocPoint.add(_allocPoint);
+        poolInfo.push(
+            PoolInfo({lpToken: _lpToken, allocPoint: _allocPoint, lastRewardBlock: lastRewardBlock, accWBANPerShare: 0})
+        );
+        updateStakingPool();
+    }
+
+    // Update the given pool's BANANA allocation point. Can only be called by the owner.
+    function set(
+        uint256 _pid,
+        uint256 _allocPoint,
+        bool _withUpdate
+    ) public onlyOwner {
+        if (_withUpdate) {
+            massUpdatePools();
+        }
+        uint256 prevAllocPoint = poolInfo[_pid].allocPoint;
+        poolInfo[_pid].allocPoint = _allocPoint;
+        if (prevAllocPoint != _allocPoint) {
+            totalAllocPoint = totalAllocPoint.sub(prevAllocPoint).add(_allocPoint);
+            updateStakingPool();
+        }
+    }
+
+    function updateStakingPool() internal {
+        uint256 length = poolInfo.length;
+        uint256 points = 0;
+        for (uint256 pid = 1; pid < length; ++pid) {
+            points = points.add(poolInfo[pid].allocPoint);
+        }
+        if (points != 0) {
+            points = points.div(3);
+            totalAllocPoint = totalAllocPoint.sub(poolInfo[0].allocPoint).add(points);
+            poolInfo[0].allocPoint = points;
+        }
+    }
+
+    // Return reward multiplier over the given _from to _to block.
+    function getMultiplier(uint256 _from, uint256 _to) public view returns (uint256) {
+        return _to.sub(_from);
+    }
+
+    // View function to see pending wBAN on frontend.
+    function pendingWBAN(uint256 _pid, address _user) external view returns (uint256) {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][_user];
+        uint256 accWBANPerShare = pool.accWBANPerShare;
+        uint256 lpSupply = pool.lpToken.balanceOf(address(this));
+        if (block.number > pool.lastRewardBlock && lpSupply != 0) {
+            uint256 multiplier = getMultiplier(pool.lastRewardBlock, block.number);
+            uint256 cakeReward = multiplier.mul(wbanRewardPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
+            accWBANPerShare = accWBANPerShare.add(cakeReward.mul(1e12).div(lpSupply));
+        }
+        return user.amount.mul(accWBANPerShare).div(1e12).sub(user.rewardDebt);
+    }
+
+    // Deposit Rewards into contract
+    function depositRewards(uint256 _amount) external {
+        require(_amount > 0, 'Deposit value must be greater than 0.');
+        wban.safeTransferFrom(address(msg.sender), address(this), _amount);
+        emit DepositRewards(_amount);
+    }
+
+    // Update reward variables for all pools. Be careful of gas spending!
+    function massUpdatePools() public {
+        uint256 length = poolInfo.length;
+        for (uint256 pid = 0; pid < length; ++pid) {
+            updatePool(pid);
+        }
+    }
+
+    // Update reward variables of the given pool to be up-to-date.
+    function updatePool(uint256 _pid) public validatePool(_pid) {
+        PoolInfo storage pool = poolInfo[_pid];
+        if (block.number <= pool.lastRewardBlock) {
+            return;
+        }
+        uint256 lpSupply = pool.lpToken.balanceOf(address(this));
+        if (lpSupply == 0) {
+            pool.lastRewardBlock = block.number;
+            return;
+        }
+        uint256 multiplier = getMultiplier(pool.lastRewardBlock, block.number);
+        uint256 cakeReward = multiplier.mul(wbanRewardPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
+        pool.accWBANPerShare = pool.accWBANPerShare.add(cakeReward.mul(1e12).div(lpSupply));
+        pool.lastRewardBlock = block.number;
+    }
+
+    // Deposit LP tokens to MasterApe for BANANA allocation.
+    function deposit(uint256 _pid, uint256 _amount) public validatePool(_pid) {
+        require(_pid != 0, 'deposit BANANA by staking');
+
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][msg.sender];
+        updatePool(_pid);
+        if (user.amount > 0) {
+            uint256 pending = user.amount.mul(pool.accWBANPerShare).div(1e12).sub(user.rewardDebt);
+            if (pending > 0) {
+                wban.safeTransfer(msg.sender, pending);
+            }
+        }
+        if (_amount > 0) {
+            pool.lpToken.safeTransferFrom(address(msg.sender), address(this), _amount);
+            user.amount = user.amount.add(_amount);
+        }
+        user.rewardDebt = user.amount.mul(pool.accWBANPerShare).div(1e12);
+        emit Deposit(msg.sender, _pid, _amount);
+    }
+
+    // Withdraw LP tokens from MasterApe.
+    function withdraw(uint256 _pid, uint256 _amount) public validatePool(_pid) {
+        require(_pid != 0, 'withdraw BANANA by unstaking');
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][msg.sender];
+        require(user.amount >= _amount, 'withdraw: not good');
+
+        updatePool(_pid);
+        uint256 pending = user.amount.mul(pool.accWBANPerShare).div(1e12).sub(user.rewardDebt);
+        if (pending > 0) {
+            wban.safeTransfer(msg.sender, pending);
+        }
+        if (_amount > 0) {
+            user.amount = user.amount.sub(_amount);
+            pool.lpToken.safeTransfer(address(msg.sender), _amount);
+        }
+        user.rewardDebt = user.amount.mul(pool.accWBANPerShare).div(1e12);
+        emit Withdraw(msg.sender, _pid, _amount);
+    }
+
+    // Withdraw without caring about rewards. EMERGENCY ONLY.
+    function emergencyWithdraw(uint256 _pid) public {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][msg.sender];
+        pool.lpToken.safeTransfer(address(msg.sender), user.amount);
+        emit EmergencyWithdraw(msg.sender, _pid, user.amount);
+        user.amount = 0;
+        user.rewardDebt = 0;
+    }
+
+    function getPoolInfo(uint256 _pid)
+        public
+        view
+        returns (
+            address lpToken,
+            uint256 allocPoint,
+            uint256 lastRewardBlock,
+            uint256 accWBANPerShare
+        )
+    {
+        return (
+            address(poolInfo[_pid].lpToken),
+            poolInfo[_pid].allocPoint,
+            poolInfo[_pid].lastRewardBlock,
+            poolInfo[_pid].accWBANPerShare
+        );
+    }
+}
